72def06fc1c0da4ea807dd2afb2472faef1d6cee
SantiSvk <santisverko@gmail.com>
2023-01-21 16:00:01 -0300

    html-css-maquetado

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 <html lang="en">
 
 <head>
-    <title>Blog de comida</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Cocina</title>
 </head>
+
 
 <body>
 
     <!-- HEADER -->
 
-    <header>
+    <header id="header">
         <div id="logo">
             <a href="index.php">
                 Blog de Cocina
@@ -19,96 +24,119 @@
             <ul>
                 <li><a href="index.php"> Categoría 1 </a></li>
                 <li><a href="index.php"> Categoría 2 </a></li>
-                <li><a href="index.php"></a> Categoría 3 </li>
-                <li><a href="index.php"></a> Categoría 4 </li>
-                <li><a href="index.php"></a> Categoría 5 </li>
+                <li><a href="index.php"> Categoría 3 </a></li>
+                <li><a href="index.php"> Categoría 4 </a></li>
+                <li><a href="index.php"> Categoría 5 </a></li>
+                <li><a href="index.php"> Sobre Mí </a></li>
+                <li><a href="index.php"> Contacto </a></li>
+
             </ul>
         </nav>
+        <div class="clearfix"></div>
     </header>
 
-    <!-- SIDE BAR -->
-    <aside>
-        <div id="login" class="block-aside">
-            <p>Identificate</p>
-            <form action="login.php" method="POST">
-                <label for="email">Email</label>
-                <input type="email" name="email">
 
-                <label for="password">Contraseña</label>
-                <input type="password" name="password">
+    <div id="container">
 
-                <input type="submit" value="Entrar">
-            </form>
-        </div>
-    </aside>
+        <!-- SIDE BAR -->
+        <aside id="sidebar">
+            <div id="login" class="block-aside">
+                <h3>Identificate</h3>
+                <form action="login.php" method="POST">
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
 
-    <aside>
-        <div id="register" class="block-aside">
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password">
 
-            <p>Regístrate</p>
+                    <input type="submit" value="Entrar">
+                </form>
+            </div>
+            <div id="register" class="block-aside">
 
-            <form action="register.php" method="POST">
+                <h3>Regístrate</h3>
 
-                <label for="username">Username</label>
-                <input type="text" name="username">
+                <form action="register.php" method="POST">
 
-                <label for="email">Email</label>
-                <input type="email" name="email">
+                    <label for="username">Username</label>
+                    <input type="text" name="username">
 
-                <label for="password">Contraseña</label>
-                <input type="password" name="password">
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
 
-                <label for="password_repeat">Repite la contraseña</label>
-                <input type="password" name="password_repeat">
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password">
 
-                <input type="submit" value="Entrar">
-            </form>
-        </div>
-    </aside>
+                    <label for="password_repeat">Repite la contraseña</label>
+                    <input type="password" name="password_repeat">
 
-    <!-- MAIN -->
+                    <input type="submit" value="Entrar">
+                </form>
+            </div>
+        </aside>
 
-    <main id="principal">
-        <h1>Últimas entradas</h1>
+        <!-- MAIN -->
 
-        <article class="entry">
-            <h2 class="entry_title">Título de la entrada</h2>
-            <p class="entry_description"> 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
-            </p>
-        </article>
+        <main id="principal">
 
-        <article class="entry">
-            <h2 class="entry_title">Título de la entrada</h2>
-            <p class="entry_description"> 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
-            </p>
-        </article>
+            <h1>Últimas entradas</h1>
 
-        <article class="entry">
-            <h2 class="entry_title">Título de la entrada</h2>
-            <p class="entry_description"> 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
-            </p>
-        </article>
+            <a href="">
+                <article class="entry">
+                    <h2 class="entry_title">Título de la entrada</h2>
+                    <p class="entry_description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
+                    </p>
+                </article>
+            </a>
 
-        <article class="entry">
-            <h2 class="entry_title">Título de la entrada</h2>
-            <p class="entry_description"> 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
-            </p>
-        </article>
+            <article class="entry">
+                <a href="">
+                    <h2 class="entry_title">Título de la entrada</h2>
+                </a>
+                <p class="entry_description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
+                </p>
+            </article>
 
-        <article class="entry">
-            <h2 class="entry_title">Título de la entrada</h2>
-            <p class="entry_description"> 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
-            </p>
-        </article>
-    </main>
+            <a href="">
+                <article class="entry">
+                    <h2 class="entry_title">Título de la entrada</h2>
+                    <p class="entry_description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
+                    </p>
+                </article>
+            </a>
+
+            <a href="">
+                <article class="entry">
+                    <h2 class="entry_title">Título de la entrada</h2>
+                    <p class="entry_description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
+                    </p>
+                </article>
+            </a>
+
+            <a href="">
+                <article class="entry">
+                    <h2 class="entry_title">Título de la entrada</h2>
+                    <p class="entry_description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur dolorem fugiat illum tenetur harum nisi labore ipsa et! Eos autem modi quisquam quis corrupti veritatis dignissimos aut, laudantium numquam, sed delectus nulla? Illo quam blanditiis consectetur quidem, assumenda corporis perspiciatis est sunt? Animi sequi accusamus mollitia dolorum ut doloremque deleniti libero omnis iste harum, veniam quidem. Asperiores, exercitationem molestiae?
+                    </p>
+                </article>
+            </a>
+            <a href="">
+                <div id="ver-todas">
+                    <span href="">Ver todas las entradas</span>
+                </div>
+            </a>
+        </main>
+    </div>
+    <div class="clearfix"></div>
+
     <!-- FOOTER -->
-    <footer>
-        <p>Desarrolado por SantiSvk &copy;</p>
+    <footer id="footer">
+        <p>Desarrollado por SantiSvk &copy; <?=date("Y")?></p>
     </footer>
 
 </body>
